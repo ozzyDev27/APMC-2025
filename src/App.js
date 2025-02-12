bc0280524e85dca44827dccd5ec94d56945db092
ozzy <businessozzy27@gmail.com>
2025-02-12 14:35:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -170,7 +170,7 @@ function Awards() {
           </div>
           <div className="homeCard award secondaward">
             <h1>Enjoyment Award</h1>
-            <p>The enjoyment award is for projects that are the most fun to use or the most useful! Your project's user experience will be something to remember! You will also get a <b>3D printed trophy for this award</b></p>
+            <p>The enjoyment award is for projects that are the most fun to use or the most useful! Your project's user experience will be something to remember! You will also get a <b>3D printed trophy</b> for this award!</p>
           </div>
         </div>
       </div>
